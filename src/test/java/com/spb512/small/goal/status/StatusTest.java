48b4a9edf2a1cba74370514efb38804f216a0888
spb512 <spb512@163.com>
2022-09-25 21:47:49 +0800

    1、去掉junit4依赖。

--- src/test/java/com/spb512/small/goal/status/StatusTest.java
+++ src/test/java/com/spb512/small/goal/status/StatusTest.java
@@ -2,9 +2,9 @@ package com.spb512.small.goal.status;
 
 import com.spb512.small.goal.service.StatusService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.annotation.Resource;
@@ -16,7 +16,7 @@ import javax.annotation.Resource;
  * @date 2022年6月6日 下午11:01:53
  */
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @WebAppConfiguration
 class StatusTest {
--- src/test/java/com/spb512/small/goal/trade/TradeTests.java
+++ src/test/java/com/spb512/small/goal/trade/TradeTests.java
@@ -2,9 +2,9 @@ package com.spb512.small.goal.trade;
 
 import com.spb512.small.goal.service.TradeService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.annotation.Resource;
@@ -13,7 +13,7 @@ import javax.annotation.Resource;
  * @author spb512
  * @date 2022年6月21日 下午7:59:28
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @WebAppConfiguration
 class TradeTests {
